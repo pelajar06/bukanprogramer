f40162cb661d9d6fba3c3d35662195742e322f92
pelajar06 <syidputraja06@gmail.com>
2024-04-02 01:43:57 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+var users [
+    {
+        username: "rasyid", password: "", redirectUrl: "https://i.ibb.co/NVvdh7b/IMG-2498-110636.jpg"},
+    
+    {
+        username: "jascha", password: "", redirectUrl: "https://i.ibb.co/dGb8FRt/IMG-20240314-WA0010.jpg"},
+    
+    {
+        username: "aksansaepul", password: "", redirectUrl: "https://i.ibb.co/zSz9BGq/Screenshot-2024-04-02-00-50-01-212-com-miui-gallery.jpg"},
+    
+];
+
+function login1() {
+  var username = document.getElementById("user").value;
+  var password = document.getElementById("pass").value;
+
+ 
